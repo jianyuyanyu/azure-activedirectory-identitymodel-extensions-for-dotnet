40870402e2e7604ec798f3e96b747939608a0284
Jenny Ferries <jeferrie@microsoft.com>
2024-12-16 15:42:17 -0800

    try with smaller sizes

--- test/Microsoft.IdentityModel.JsonWebTokens.Tests/JsonWebTokenHandlerTests.cs
+++ test/Microsoft.IdentityModel.JsonWebTokens.Tests/JsonWebTokenHandlerTests.cs
@@ -3434,11 +3434,11 @@ namespace Microsoft.IdentityModel.JsonWebTokens.Tests
 
             TheoryData<JWEDecompressionTheoryData> theoryData = new TheoryData<JWEDecompressionTheoryData>();
 #if NETCOREAPP2_1
-            string strU = new string('U', 20_000_000);
-            string strUU = new string('U', 15_000_000);
+            string strU = new string('U', 2000000);
+            string strUU = new string('U', 1500000);
 #else
-            string strU = new string('U', 100_000_000);
-            string strUU = new string('U', 40_000_000);
+            string strU = new string('U', 4000000);
+            string strUU = new string('U', 3000000);
 #endif
 
             string payload = $@"{{""U"":""{strU}"", ""UU"":""{strUU}""}}";
